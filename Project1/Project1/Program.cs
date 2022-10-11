b329e6957878bd1e583703df409bdb1f9062cfaa
Horea Oros <horea.oros@gmail.com>
2022-10-11 09:32:49 +0300

    add ShowPad

--- Project1/Project1/Program.cs
+++ Project1/Project1/Program.cs
@@ -10,6 +10,41 @@ namespace Project1
     {
         static void Main(string[] args)
         {
+            Show();
+        }
+
+        private static void Show()
+        {
+            int n = 256;
+            int pad = 8;
+
+            for (int i = 0; i < n; i++)
+            {
+                ShowPad(i, pad);
+            }
+
+
+        }
+
+        private static void ShowPad(int i, int pad)
+        {
+            Stack<int> stack = new Stack<int>();
+            while (i > 0)
+            {
+                stack.Push(i % 2);
+                i = i / 2;
+            }
+
+            int z = pad - stack.Count; 
+            for (int j = 0; j < z; j++)
+            {
+                stack.Push(0);
+            }
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop());
+            }
+            Console.WriteLine();
         }
     }
 }
